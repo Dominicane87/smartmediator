216d01d7e1e146455ed4a3e35d4488872540e44d
egorov_av <egorov_av@mail.ru>
2019-12-01 19:25:28 +0300

    initial commit

---
+++ src/main/java/stc21/smartmanager/Application.java
@@ -0,0 +1,22 @@
+package stc21.smartmanager;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Hello from Smart Mediator.");
+        };
+    }
+}
---
+++ src/main/java/stc21/smartmanager/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package stc21.smartmanager.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Smart Mediator.";
+    }
+}
